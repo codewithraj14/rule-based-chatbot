e101f33ebc5bc0c44db8adb803ffcba06b32789b
codewithraj14 <rajrishabh9559@gmail.com>
2026-03-31 13:44:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+ #Rule Based AI Python Chatbot
+
+import datetime
+import time
+
+name= input("swagat h, enter your name : ")
+presentHour= datetime.datetime.now().hour
+
+if 5 <= presentHour <= 11:
+    print("Good morning,", name)
+elif 11 <= presentHour <= 17:
+    print("Good afternoon,", name)
+elif 17 <= presentHour <= 20:
+    print("Good evening,",name)
+else:
+    print("Good night,",name)          
+
+print("namaste! Welcome to Your chatBot")
+print("You can ask me basic question, Type 'bye' to exit from the bot")
+
+# Chatbot Memory Creation [ dictionary of responses ]
+
+responses = {
+    "hello": "Hi, welcome. How can I help you?",
+    "how are you": "I am very fine. Thank you",
+    "who are you": "I am smart AI chatbot",
+    "motivate me": "Keep going. Every bug of your project makes you a better developer",
+    "happy": "Great to hear that",
+    "functions kya hote hai":"jakar chapter 7 padho"  
+}
+
+# Method/Funtion to get response of chatbot
+
+def getResponsebot(userQuestion):
+    userQuestion= userQuestion.lower()
+    for eachKey in responses:
+        if eachKey in userQuestion:
+            return responses[eachKey]
+        
+    
+    
+
+# Take user input 
+
+
+while True:
+  userInput= input("Please ask your question:")
+  reply= getResponsebot(userInput)
+  print("Bot Response :", reply)
+
+  if "bye" in userInput.lower():
+      break
